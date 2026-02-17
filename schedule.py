889630ff83a5c94dccbc1ba3ec71213d529fdef5
456i <timofeymarushko@gmail.com>
2026-02-17 10:58:44 +0300

    added feature priority_notifications

--- schedule.py
+++ schedule.py
@@ -1,23 +1,46 @@
+from dataclasses import dataclass
 from datetime import datetime
+from typing import Literal
+
+
+@dataclass(frozen=True)
+class Notification:
+    id: int
+    user_id: str
+    message: str
+    send_at: datetime
+    priority: Literal["high", "normal"] = "normal"
 
 
 class Scheduler:
     def __init__(self):
-        self.__id = 0
-        self.__storage: list[dict] = []
+        self.__id: int = 0
+        self.__storage: list[Notification] = []
 
-    def schedule(self, user_id: int, message: str, send_at: datetime):
-        self.__storage.append(
-            {
-                "id": self.__id,
-                "user_id": user_id,
-                "message": message,
-                "send_at": send_at,
-            }
+    def schedule(
+        self,
+        user_id: str,
+        message: str,
+        send_at: datetime,
+        priority: Literal["high", "normal"] = "normal",
+    ) -> None:
+        notification = Notification(
+            id=self.__id,
+            user_id=user_id,
+            message=message,
+            send_at=send_at,
+            priority=priority,
         )
+
+        self.__storage.append(notification)
         self.__id += 1
+
+    def run_pending(self):
+        self.__storage.sort(key=lambda n: (n.priority != "high"))
         now = datetime.now()
-        for n in self.__storage:
-            if n["send_at"] <= now:
-                print(f"{n["user_id"]} {n["message"]}")
-                self.__storage.remove(n)
+
+        ready_to_send = [n for n in self.__storage if n.send_at <= now]
+        self.__storage = [n for n in self.__storage if n.send_at > now]
+
+        for n in ready_to_send:
+            print(f"{n.user_id} {n.message}")
