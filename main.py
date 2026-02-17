e09ef334154ad634313485f40c9f9a6a3cd4352e
456i <timofeymarushko@gmail.com>
2026-02-17 10:59:49 +0300

    corrected tests

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from datetime import datetime
 
-from .schedule import Scheduler
+from schedule import Scheduler
 
 scheduler = Scheduler()
+now = datetime.now()
 
-scheduler.schedule("a1b2-c3d4", "Hello!", datetime.now())
+scheduler.schedule("a1b2g-c3d4hf", "Hello!1", now)
+scheduler.schedule("a345h-cs3d4h", "Hello!2", now)
+scheduler.schedule("a232s-c3scfg", "Hello!3", now, "high")
+scheduler.schedule("a1b2h-c3scd4", "Hello!4", now, "high")
+scheduler.run_pending()
 
-scheduler.schedule("x9y8-z7w6", "Future message", datetime(2026, 3, 1, 12, 0))
+scheduler.schedule("x56u8-z657w6", "Future message1", datetime(2026, 3, 1, 12, 0))
+scheduler.schedule("x9y8j-z7w6yj", "Future message2", datetime(2026, 3, 1, 12, 0))
+scheduler.run_pending()
