9d4cb4926998602563b01c12fee0f75d00ecde97
456i <timofeymarushko@gmail.com>
2026-03-01 19:21:32 +0300

    rest api; fast api; postgresql; pydantic; async.

---
+++ src/database/__init__.py
@@ -0,0 +1,3 @@
+from .connection import Base, get_session, lifespan
+
+__all__ = ["Base", "get_session", "lifespan"]
---
+++ src/services/__init__.py
@@ -0,0 +1,10 @@
+from .schemas import (
+    IdSchema,
+    OrderCreateSchema,
+    OrderSchema,
+    ProductCreateSchema,
+    ProductSchema,
+    UserCreateSchema,
+    UserSchema,
+)
+from .tables import Order, Product, User
